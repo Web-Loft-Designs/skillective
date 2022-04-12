51ad19d983345a7c8b97fe38917b9884e697a3d6
stellar-dev <stellardev@stellar-soft.com>
2022-04-12 18:45:03 +0300

    SKIL-862 add name sender to notification

--- resources/views/emails/footer.blade.php
+++ resources/views/emails/footer.blade.php
@@ -1,4 +1,9 @@
 <p>
     Regards,<br/>
     <a href="{{ config('app.url') }}" target="_blank">{{ config('app.name') }}</a>
+
+    <br />
+    <br />
+
+    {{ $sender_first_name ?? '' }} {{ $sender_last_name ?? '' }}
 </p>
