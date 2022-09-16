08c79925199210ea0c3ac54f2cc29e12215c1649
Anton Kozhukhar <anton.kozhukhar@gmail.com>
2022-09-16 15:10:21 +0300

    Task: [SKILL-958] Refactor

--- resources/js/services/cartService.js
+++ resources/js/services/cartService.js
@@ -1,79 +1,85 @@
-import axios from "axios";
-import guestCartHelper from "../helpers/guestCartHelper";
+import axios from 'axios'
+import guestCartHelper from '../helpers/guestCartHelper'
 
 const cartService = {
-    async fetchCartItems() {
-        const response = await axios
-          .get(
-            '/api/cart',
-            { params: { guest_cart: guestCartHelper.getProductsString() } }
-          )
-          .catch(e => console.log(e))
-        return response.data.data.data
-    },
-    async addItemToCartAtStart(data, guestMode) {
-        if (guestMode) {
-            return guestCartHelper.addProduct({
-                lesson_id: data.lessonId,
-                description: data.specialRequest,
-                isPreRecorded: data.isPreRecorded
-            });
-        } else {
-            const response = await axios
-                .post("/api/cart", {
-                    lesson_id: data.lessonId,
-                    description: data.specialRequest,
-                    isPreRecorded: data.isPreRecorded
-                })
-                .catch(({ response }) => {
-                    return { message: response.data.message, isError: true };
-                });
-            return response;
+  async fetchCartItems() {
+    const response = await axios
+      .get(
+        '/api/cart',
+        {
+          params: {
+            guest_cart: guestCartHelper.getProductsString()
+          }
         }
-    },
-    async removeItemFromCart(id, guestMode) {
-        if (guestMode || guestCartHelper.isLessonInGuestCart(id)) {
-            guestCartHelper.removeProduct(id);
-        } else {
-            await axios.delete("/api/cart/" + id).catch(e => {
-                console.log(e);
-            });
-        }
-    },
-    async checkCartDotNeeded(guestMode) {
-        let needed = false;
-        if (guestMode) {
-            needed = !guestCartHelper.isEmpty();
-        } else {
-            needed = await axios
-                .get(
-                    `/api/cart/has-items?guest_cart=${guestCartHelper.getProductsString()}`
-                )
-                .catch(e => {
-                    console.log(e);
-                });
-            needed = needed.data.data;
-            // guestCartHelper.clearProducts();
-        }
-        return needed;
-    },
-    async fetchCartTotal(guestMode) {
-        if (guestMode) {
-            const response = await axios
-                .get(
-                    `/api/cart/total?guest_cart=${guestCartHelper.getProductsString()}&promo_codes=${guestCartHelper.getPromosString()}`
-                )
-                .catch(e => {
-                    console.log(e);
-                });
-            return response.data.data;
-        } else {
-            const response = await axios.get(`/api/cart/total?guest_cart=${guestCartHelper.getProductsString()}&promo_codes=${guestCartHelper.getPromosString()}`).catch(e => {
-                console.log(e);
-            });
-            return response.data.data;
-        }
+      ).catch(e => console.log(e))
+    return response.data.data.data
+  },
+  async addItemToCartAtStart(data, guestMode) {
+    if (guestMode) {
+      return guestCartHelper.addProduct({
+        lesson_id: data.lessonId,
+        description: data.specialRequest,
+        isPreRecorded: data.isPreRecorded
+      })
+    } else {
+      return await axios
+        .post('/api/cart', {
+          lesson_id: data.lessonId,
+          description: data.specialRequest,
+          isPreRecorded: data.isPreRecorded
+        }).catch(({ response }) => {
+          return { message: response.data.message, isError: true }
+        })
     }
-};
+  },
+  async removeItemFromCart(id, guestMode) {
+    guestMode || guestCartHelper.isLessonInGuestCart(id)
+      ? guestCartHelper.removeProduct(id)
+      : await axios.delete('/api/cart/' + id).catch(e => console.log(e))
+  },
+  async checkCartDotNeeded(guestMode) {
+    let needed = false
+    if (guestMode) {
+      needed = !guestCartHelper.isEmpty()
+    } else {
+      needed = await axios.get(
+        '/api/cart/has-items',
+        {
+          params: {
+            guest_cart: guestCartHelper.getProductsString()
+          }
+        }
+      ).catch(e => console.log(e))
+      needed = needed.data.data
+      // guestCartHelper.clearProducts();
+    }
+    return needed
+  },
+  async fetchCartTotal(guestMode) {
+    if (guestMode) {
+      const response = await axios.get(
+        '/api/cart/total',
+        {
+          params: {
+            guest_cart: guestCartHelper.getProductsString(),
+            promo_codes: guestCartHelper.getPromosString()
+          }
+        }
+      ).catch(e => console.log(e))
+      return response.data.data
+    } else {
+      const response = await axios.get(
+        '/api/cart/total',
+        {
+          params: {
+            guest_cart: guestCartHelper.getProductsString(),
+            promo_codes: guestCartHelper.getPromosString()
+          }
+        }
+      ).catch(e => console.log(e))
+      return response.data.data
+    }
+  }
+}
 
-export default cartService;
+export default cartService
