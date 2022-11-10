cc3eea1ddf873ebb7b64b3c022fa7ff0609c9c58
aleksvin8888 <aleksvin8888@gmail.com>
2022-11-10 10:52:47 +0200

    Task:[Skl-124] Add legal_name to users table

--- database/migrations/2022_11_10_083707_add_column_tax_id_legal_name_to_users_table.php
+++ database/migrations/2022_11_10_083707_add_column_tax_id_legal_name_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnTaxIdToUsersTable extends Migration
+class AddColumnTaxIdLegalNameToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddColumnTaxIdToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('tax_id', 200)->nullable()->after('email');
+            $table->string('tax_id', 15)->nullable()->after('email');
+            $table->string('legal_name', 50)->nullable()->after('tax_id');
         });
     }
 
