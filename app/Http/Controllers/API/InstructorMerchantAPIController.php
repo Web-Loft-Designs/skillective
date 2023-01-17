f0de4c3d016069d13b34dc4e6e331e246b9f1a44
aleksvin8888 <aleksvin8888@gmail.com>
2023-01-17 16:55:02 +0200

    Task:[Skl-139] Hot fix taxId and ssn

--- app/Http/Controllers/API/InstructorMerchantAPIController.php
+++ app/Http/Controllers/API/InstructorMerchantAPIController.php
@@ -39,35 +39,32 @@ class InstructorMerchantAPIController extends AppBaseController
 			}
 			return $this->sendError('Can\'t create Merchant Account', 400);
 		}catch (\Exception $e){
+            Log::info($e);
 			return $this->sendError($e->getMessage(), 400);
 		}
     }
 
 	public function update(BraintreeUpdateMerchantRequest $request)
 	{
-        $request->taxId = str_replace(['-',',','/','.',';',':',' ','"', "'"], '', trim($request->taxId));
+        $request['taxId'] = str_replace(['-',',','/','.',';',':',' ','"', "'"], '', trim($request->taxId));
+        if(strlen($request->taxId) == 9) {
+            $request['individual_ssn'] = $request->taxId;
+        }
 
 		$user = Auth::user();
 		try{
-
 			$merchantAccount = BraintreeProcessor::updateMerchant($user, $request);
-
             if ($merchantAccount != false)
             {
-
                 if($request->taxId !== null) {
                     $user->update([
                         'tax_id' => $request->taxId,
                         'legal_name' => $request->legalName,
                     ]);
                 }
-
-
                 return $this->sendResponse(BraintreeProcessor::_prepareMerchantAccountOutput($merchantAccount), 'Merchant account created and will be verified soon');
             }
-
 		}catch (\Exception $e){
-
 			Log::info($e);
 			return $this->sendError($e->getMessage(), 400);
 		}
