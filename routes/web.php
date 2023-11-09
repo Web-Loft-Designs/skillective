effb41498068d5163c8d6650d5af4d3bbb17b1d5
AlexandrKomarnitskyi <a.komarnitskyi@devocean.pro>
2023-11-09 12:34:09 +0200

    Task:[SKL-1021-(241)] fix commit

--- routes/web.php
+++ routes/web.php
@@ -28,12 +28,12 @@ Route::get('/lesson/{lesson}', 'LessonPageController@index')->name('lesson');
 Route::post('/braintree/webhook', 'BraintreeWebhookController@index')->middleware(['guest']);
 
 //Auth::routes();
-Route::group(['middleware'=>['guest']], function () {
+Route::group(['middleware'=>['guest', 'web']], function () {
 // frontend users login
 	Route::get('login', 'Auth\FrontendLoginController@showLoginForm')->name('frontend.login');
 	Route::post('login', 'Auth\FrontendLoginController@login')->name('frontend.login');
 // logging in for Admins
-	Route::get('backend/login', 'Auth\BackendLoginController@showLoginForm')->name('login')->middleware(['rememberHttpReferer']);
+	Route::get('backend/login', 'Auth\BackendLoginController@showLoginForm')->name('login')->middleware(['rememberHttpReferer', 'web']);
 	Route::post('backend/login', 'Auth\BackendLoginController@login')->name('login');
 // Instructor registration
 	Route::get('instructor/register', [InstructorRegisterController::class, 'showRegistrationForm'])->name('instructor.register');
@@ -54,8 +54,8 @@ Route::group(['middleware'=>['guest']], function () {
 // Logout
 Route::post('logout', 'Auth\FrontendLoginController@logout')->name('logout');
 
-Route::get('/profile/edit/{user}', 'ProfileController@edit')->name('profile.edituser')->middleware(['role:Admin']); // edit user profile
-Route::get('/profile/edit', 'ProfileController@edit')->name('profile.edit')->middleware(['role:Instructor|Student']); // edit own profile
+Route::get('/profile/edit/{user}', 'ProfileController@edit')->name('profile.edituser')->middleware(['role:Admin', 'web']); // edit user profile
+Route::get('/profile/edit', 'ProfileController@edit')->name('profile.edit')->middleware(['role:Instructor|Student',  'web']); // edit own profile
 Route::get('/profile/{user?}', 'ProfileController@show')->name('profile'); // user public profile
 
 // Socialite Register Routes
@@ -63,12 +63,12 @@ Route::get('/social/redirect/{provider}', [SocialController::class, 'getSocialRe
 Route::get('/social/handle/{provider}', [SocialController::class, 'getSocialHandle'])->name('social.handle');
 Route::get('/social/{provider}/instructor/registration', [SocialController::class, 'getSocialInstructorRegistration'])
     ->name('social.instructor.registration')
-    ->middleware(['guest']);
+    ->middleware(['guest' ,  'web']);
 Route::get('/social/ig/load-media', [SocialController::class, 'getSocialInstagramMedia'])
     ->name('social.instagram.media.update')
-    ->middleware(['role:Instructor']);
+    ->middleware(['role:Instructor',  'web']);
 Route::post('/social/detach/{provider}', [SocialController::class, 'detachSocial'])
-    ->middleware(['auth'])
+    ->middleware(['auth',  'web'])
     ->name('social.detach');
 
 
@@ -76,7 +76,7 @@ Route::get('/globalshop', [GlobalShopController::class, 'index'])->name('globals
 Route::get('/checkout', 'CheckoutController@index')->name('checkout');
 Route::get('/cart', 'CartController@index')->name('cart');
 
-Route::group(['prefix' => 'instructor', 'middleware'=>['role:Instructor']], function () {
+Route::group(['prefix' => 'instructor', 'middleware'=>['role:Instructor',  'web']], function () {
 	Route::get('/dashboard', 'InstructorDashboardController@index')->name('instructor.dashboard');
 	Route::get('/schedule', 'InstructorScheduleController@index')->name('instructor.schedule');
 	Route::get('/bookings', 'InstructorBookingsController@index')->name('instructor.bookings');
@@ -94,7 +94,7 @@ Route::group(['prefix' => 'instructor', 'middleware'=>['role:Instructor']], func
 	Route::get('/discount-management', 'InstructorDiscountManagementController@index')->name('instructor.discount-management');
 });
 
-Route::group(['prefix' => 'student', 'middleware'=>['role:Student']], function () {
+Route::group(['prefix' => 'student', 'middleware'=>['role:Student',  'web']], function () {
 	Route::get('/dashboard', 'StudentDashboardController@index')->name('student.dashboard');
 	Route::get('/schedule', 'StudentScheduleController@index')->name('student.schedule');
 	Route::get('/bookings', 'StudentBookingsController@index')->name('student.bookings');
@@ -108,14 +108,14 @@ Route::group(['prefix' => 'student', 'middleware'=>['role:Student']], function (
 
 
 // Virtual lessons
-Route::post('lesson/room-events', 'VirtualLessonRoomEventsController@handle')->name('twiliocallbackhandler')->middleware(['guest']);
+Route::post('lesson/room-events', 'VirtualLessonRoomEventsController@handle')->name('twiliocallbackhandler')->middleware(['guest',  'web']);
 // open videochat window
-Route::group(['middleware'=>['role:Instructor|Student']], function () {
+Route::group(['middleware'=>['role:Instructor|Student',  'web']], function () {
     Route::get('/virtual-lesson/{lesson}', 'VirtualLessonPageController@index')->name('virtual-lesson');
 });
 
 // BACKEND ROUTES
-Route::group(['prefix' => 'backend', 'middleware'=>['rememberHttpReferer', 'role:Admin']], function () {
+Route::group(['prefix' => 'backend', 'middleware'=>['rememberHttpReferer', 'role:Admin',  'web']], function () {
 
 	Route::post('/login-as', 'Backend\LoginAsController@loginAs');
 
