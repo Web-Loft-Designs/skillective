b6e029308023931df701108cfb75adb205d7a50e
Maksym <tsymbal12321@gmail.com>
2023-11-20 13:12:25 +0200

    Add paypal-js

--- resources/js/components/LessonBookingFormPp.vue
+++ resources/js/components/LessonBookingFormPp.vue
@@ -1,145 +1,171 @@
 <template>
-    <div>
-        <div id="paypal-button-container"></div>
-
-        <div v-if="isHostedFieldsEligible" class="card_container">
-            <form id="card-form" @submit.prevent="submitCardForm">
-                <label for="card-number">Card Number</label>
-                <div id="card-number" class="card_field"></div>
-
-                <div>
-                    <label for="expiration-date">Expiration Date</label>
-                    <div id="expiration-date" class="card_field"></div>
-                </div>
-
-                <div>
-                    <label for="cvv">CVV</label>
-                    <div id="cvv" class="card_field"></div>
-                </div>
-
-                <label for="card-holder-name">Name on Card</label>
-                <input type="text" v-model="cardHolderName" id="card-holder-name" name="card-holder-name" autocomplete="off" placeholder="card holder name" />
-
-                <!-- Other billing address fields... -->
-
-                <br /><br />
-                <button type="submit" class="btn">Pay</button>
-            </form>
-        </div>
+  <div>
+    <div id='paypal-button-container'>
+      <div id='your-container-element' class='your-container-element'></div>
     </div>
+
+    <div v-if='isHostedFieldsEligible' class='card_container'>
+      <form id='card-form' @submit.prevent='submitCardForm'>
+        <label for='card-number'>Card Number</label>
+        <div id='card-number' class='card_field'></div>
+
+        <div>
+          <label for='expiration-date'>Expiration Date</label>
+          <div id='expiration-date' class='card_field'></div>
+        </div>
+
+        <div>
+          <label for='cvv'>CVV</label>
+          <div id='cvv' class='card_field'></div>
+        </div>
+
+        <label for='card-holder-name'>Name on Card</label>
+        <input
+          id='card-holder-name'
+          v-model='cardHolderName'
+          autocomplete='off'
+          name='card-holder-name'
+          placeholder='card holder name'
+          type='text'
+        />
+
+        <!-- Other billing address fields... -->
+
+        <br/><br/>
+        <button class='btn' type='submit'>Pay</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
+import { loadScript } from '@paypal/paypal-js'
+
+let paypal
 export default {
-    data() {
-        return {
-            isHostedFieldsEligible: false,
-            orderId: null,
-            cardHolderName: '',
-            vaultSetupToken: null,
-        };
-    },
-    mounted() {
-        this.initializePayPal();
-    },
-    methods: {
-        initializePayPal() {
-            paypal
-                .Buttons({
-                    createOrder: () => {
-                        return this.createOrder();
-                    },
-                    onApprove: (data) => {
-                        return this.captureOrder(data.orderID);
-                    },
-                })
-                .render('#paypal-button-container');
+  name: 'LessonBookingFormPp',
+  data() {
+    return {
+      isHostedFieldsEligible: false,
+      orderId: null,
+      cardHolderName: '',
+      vaultSetupToken: null
+    }
+  },
+  props: [
+    'ppClientToken', 'total'
+  ],
+  mounted() {
+    console.log(this.ppClientToken, 'ppClientToken')
+    console.log(this.total, 'total')
+    this.initializePayPal()
+  },
+  methods: {
+    async initializePayPal() {
+      let paypal;
 
-            if (paypal.HostedFields.isEligible()) {
-                this.isHostedFieldsEligible = true;
+      try {
+        paypal = await loadScript({ clientId: this.ppClientToken, components: ['buttons','hosted-fields','funding-eligibility'] });
+      } catch (error) {
+        console.error("failed to load the PayPal JS SDK script", error);
+      }
 
-                paypal.HostedFields.render({
-                    createOrder: () => {
-                        return this.createOrder();
+      if (paypal) {
+        try {
+          await paypal.Buttons().render("#your-container-element");
+        } catch (error) {
+          console.error("failed to render the PayPal Buttons", error);
+        }
+      }
+      console.log(1)
+      console.log(paypal)
+      console.log(paypal.HostedFields.isEligible(), 'paypal.HostedFields.isEligible()')
+      console.log(paypal.HostedFields.render, 'paypal.HostedFields.render()')
+        if (paypal.HostedFields.isEligible()) {
+            this.isHostedFieldsEligible = true;
+
+            paypal.HostedFields.render({
+                createOrder: () => {
+                    return this.createOrder();
+                },
+                styles: {
+                    '.valid': {
+                        color: 'green',
                     },
-                    styles: {
-                        '.valid': {
-                            color: 'green',
-                        },
-                        '.invalid': {
-                            color: 'red',
-                        },
+                    '.invalid': {
+                        color: 'red',
                     },
-                    fields: {
-                        number: {
-                            selector: '#card-number',
-                            placeholder: '4111 1111 1111 1111',
-                        },
-                        cvv: {
-                            selector: '#cvv',
-                            placeholder: '123',
-                        },
-                        expirationDate: {
-                            selector: '#expiration-date',
-                            placeholder: 'MM/YY',
-                        },
+                },
+                fields: {
+                    number: {
+                        selector: '#card-number',
+                        placeholder: '4111 1111 1111 1111',
                     },
-                }).then((cardFields) => {
-                    this.cardFields = cardFields;
-                });
-            }
-        },
-        createOrder() {
-            // Implement logic to create an order and return the order ID
-            return fetch('/api/orders', {
-                method: 'post',
-                body: JSON.stringify({
-                    cart: [
-                        {
-                            sku: 'YOUR_PRODUCT_STOCK_KEEPING_UNIT',
-                            quantity: 'YOUR_PRODUCT_QUANTITY',
-                        },
-                    ],
-                }),
+                    cvv: {
+                        selector: '#cvv',
+                        placeholder: '123',
+                    },
+                    expirationDate: {
+                        selector: '#expiration-date',
+                        placeholder: 'MM/YY',
+                    },
+                },
+            }).then((cardFields) => {
+                this.cardFields = cardFields;
+            });
+        }
+    },
+    createOrder() {
+        // Implement logic to create an order and return the order ID
+        return fetch('/api/orders', {
+            method: 'post',
+            body: JSON.stringify({
+                cart: [
+                    {
+                        sku: 'YOUR_PRODUCT_STOCK_KEEPING_UNIT',
+                        quantity: 'YOUR_PRODUCT_QUANTITY',
+                    },
+                ],
+            }),
+        })
+            .then((res) => res.json())
+            .then((order) => {
+                this.orderId = order.id;
+                return order.id;
+            });
+    },
+    captureOrder(orderId) {
+        // Implement logic to capture the order
+        return fetch(`/api/orders/${orderId}/capture`, {
+            method: 'post',
+        })
+            .then((res) => res.json())
+            .then((orderData) => {
+                // Handle capture response
+            });
+    },
+    submitCardForm() {
+        this.cardFields
+            .submit({
+                cardholderName: this.cardHolderName,
+                // Other billing address fields...
             })
-                .then((res) => res.json())
-                .then((order) => {
-                    this.orderId = order.id;
-                    return order.id;
-                });
-        },
-        captureOrder(orderId) {
-            // Implement logic to capture the order
-            return fetch(`/api/orders/${orderId}/capture`, {
-                method: 'post',
+            .then(() => {
+                this.captureOrder(this.orderId);
             })
-                .then((res) => res.json())
-                .then((orderData) => {
-                    // Handle capture response
-                });
-        },
-        submitCardForm() {
-            this.cardFields
-                .submit({
-                    cardholderName: this.cardHolderName,
-                    // Other billing address fields...
-                })
-                .then(() => {
-                    this.captureOrder(this.orderId);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    // Handle errors
-                });
-        },
-
-        handleApprove(vaultSetupToken) {
-            // Викликається при схваленні оплати
-            this.vaultSetupToken = vaultSetupToken;
-            // Додайте вашу логіку для використання vaultSetupToken
-        },
+            .catch((err) => {
+                console.error(err);
+                // Handle errors
+            });
     },
-};
+
+    handleApprove(vaultSetupToken) {
+        // Викликається при схваленні оплати
+        this.vaultSetupToken = vaultSetupToken;
+        // Додайте вашу логіку для використання vaultSetupToken
+    },
+  }
+}
 </script>
 
 <style>
--- resources/views/frontend/checkout.blade.php
+++ resources/views/frontend/checkout.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app-frontend')
 @section('content')
-{{--    <?php--}}
-{{--    dd($total, $vaultSetupToken, $ppClientToken);--}}
-{{--    ?>--}}
+{{--   <?php   --}}
+{{-- dd($ppClientToken);  --}}
+ {{--  ?>   --}}
     <div class="dashboard-cart-page">
         <div class="container dashboard-cart">
             <div class="row">
@@ -10,28 +10,21 @@
                 <div class="col-12 register-banner-wrapper" style="box-shadow: none;">
                     <checkout>
                         {{--          Braintree              --}}
-{{--                        <lesson-booking-form--}}
-{{--                            :lessons-count="{{ json_encode($lessonsCount) }}"--}}
-{{--                            :us-states="{{ json_encode($usStates) }}"--}}
-{{--                            :user="{{ json_encode($user) }}"--}}
-{{--                            :user-payment-methods="{{ json_encode($userPaymentMethods) }}"--}}
-{{--                            :client-token="'{{ $clientToken }}'"--}}
-{{--                            :payment-environment="'{{ $paymentEnvironment }}'"--}}
-{{--                            :confirmation-text="'{{ str_replace(["\r", "\n"], '', $settings['booking_confirmation_text']) }}'"--}}
-{{--                            :ppAccessToken = "'{{$ppAccessToken}}'">--}}
-{{--                        </lesson-booking-form>--}}
+                  {{--         <lesson-booking-form --}}
+         {{--      {{--                   :lessons-count="{{ json_encode($lessonsCount) }}" --}}
+                 {{--               :us-states="{{ json_encode($usStates) }}" --}}
+                        {{--        :user="{{ json_encode($user) }}" --}}
+ {{--                               :user-payment-methods="{{ json_encode($userPaymentMethods) }}" --}}
+         {{--                       :client-token="'{{ $clientToken }}'" --}}
+                 {{--               :payment-environment="'{{ $paymentEnvironment }}'" --}}
+                         {{--       :confirmation-text="'{{ str_replace(["\r", "\n"], '', $settings['booking_confirmation_text']) }}'" --}}
+ {{--                               :ppAccessToken = "'{{$ppAccessToken}}'"> --}}
+         {{--                   </lesson-booking-form> --}}
                         {{--          Paypal             --}}
                         <lesson-booking-form-pp
-                            :clientId="'your-client-id'"
-                            :merchantId="'your-merchant-id'"
-                            :currency="'USD'"
-                            :cartItems="[
-                            { sku: 'product-1', quantity: 2 },
-                            { sku: 'product-2', quantity: 1 }
-                            ]">
-
+                        :total="{{json_encode($total)}}"
+                        :pp-client-token="'{{$ppClientToken}}'">
                         </lesson-booking-form-pp>
-
                     </checkout>
                 </div>
             </div>
@@ -40,6 +33,5 @@
 @endsection
 
 @section('scripts')
-{{--    @include('frontend.partials.payment-processing-scripts') --}}{{--          Braintree              --}}
-    @include('frontend.partials.pp-payment-processing-scripts') {{--          Paypal             --}}
+   {{--   @include('frontend.partials.payment-processing-scripts')      Braintree              --}}
 @endsection
