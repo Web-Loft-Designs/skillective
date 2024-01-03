65acb9198883f0318926d34429ea24676d7a69ef
AlexandrKomarnitskyi <a.komarnitskyi@devocean.pro>
2024-01-03 14:49:15 +0200

    Task:[SKL-1021-(241)] small fixe

--- config/paypal.php
+++ config/paypal.php
@@ -6,14 +6,12 @@ return [
         'client_id'         => env('PAYPAL_SANDBOX_CLIENT_ID', ''),
         'client_secret'     => env('PAYPAL_SANDBOX_CLIENT_SECRET', ''),
         'app_id'            => env('PAYPAL_SANDBOX_APP_ID', ''),
-        'master_partner_id' => env('PAYPAL_SANDBOX_MASTER_PARTNER_ID', ''),
         'bn_code'           => env('PAYPAL_SANDBOX_BN_CODE', '')
     ],
     'live' => [
         'client_id'         => env('PAYPAL_LIVE_CLIENT_ID', ''),
         'client_secret'     => env('PAYPAL_LIVE_CLIENT_SECRET', ''),
         'app_id'            => env('PAYPAL_LIVE_APP_ID', ''),
-        'master_partner_id' => env('PAYPAL_LIVE_MASTER_PARTNER_ID', ''),
         'bn_code'           => env('PAYPAL_LIVE_BN_CODE', '')
     ],
 
